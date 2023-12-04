ae5ec6538d4448ea20674c5f8791cbd1fffd05bd
lyphrowny <strukov.as@edu.spbstu.ru>
2023-12-04 22:23:27 +0300

    Add essential db queries

--- docs/other_artifacts/db/db_queries.sql
+++ docs/other_artifacts/db/db_queries.sql
@@ -1,3 +1,8 @@
--- Get all the users, that registered via a referral link of a particular user
+-- Get all the users (and their number), that registered via a referral link of a particular user
+SELECT u.id, COUNT(u.id) FROM users as u WHERE u.invited_by = "$1";
+
 -- Get all the messages for a chat
--- Get all the chats
+SELECT * FROM messages as m WHERE m.chat_id = "$1" ORDER BY m.created_at DESC;
+
+-- Get all the chats for a particular user
+SELECT id FROM chats as ch WHERE ch.user_login = "$1";
